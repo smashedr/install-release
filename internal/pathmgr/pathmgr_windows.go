30b59eb7097aef456b2f886d102cf45c65b4e16f
Shane <6071159+smashedr@users.noreply.github.com>
2026-01-31 23:25:07 -0800

    Update pathmgr_windows.go

--- internal/pathmgr/pathmgr_windows.go
+++ internal/pathmgr/pathmgr_windows.go
@@ -3,21 +3,21 @@
 package pathmgr
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 	"syscall"
 	"unsafe"
 )
-
-//const (
-//	PathTypeSystem = 0
-//	PathTypeUser   = 1
-//)
 
 func IsDirInPath(dirPath string) (found bool, pathType uint32, err error) {
 	var findType uint32
 	fmt.Printf("IsDirInPath: %v\n", dirPath)
 
-	pathMgr := syscall.NewLazyDLL("PathMgr.dll")
+	dllPath, _ := getDLLPath()
+	fmt.Printf("dllPath: %v\n", dllPath)
+	pathMgr := syscall.NewLazyDLL(dllPath)
 	isDirInPath := pathMgr.NewProc("IsDirInPath")
 
 	dirPathPtr, err := syscall.UTF16PtrFromString(dirPath)
@@ -31,7 +31,7 @@ func IsDirInPath(dirPath string) (found bool, pathType uint32, err error) {
 			uintptr(i),
 			uintptr(unsafe.Pointer(&findType)),
 		)
-		fmt.Printf("ret: %v\n", ret)
+		//fmt.Printf("ret: %v\n", ret)
 		switch ret {
 		case 0:
 			return true, i, nil
@@ -48,7 +48,10 @@ func AddDirToPath(dirPath string, pathType, addType int) (bool, int, error) {
 	fmt.Printf("pathType: %v\n", pathType)
 	fmt.Printf("addType: %v\n", addType)
 
-	pathMgr := syscall.MustLoadDLL("PathMgr.dll")
+	dllPath, _ := getDLLPath()
+	fmt.Printf("dllPath: %v\n", dllPath)
+
+	pathMgr := syscall.MustLoadDLL(dllPath)
 	//defer func() { _ = pathMgr.Release() }()
 	addDirToPath := pathMgr.MustFindProc("AddDirToPath")
 
@@ -62,8 +65,10 @@ func AddDirToPath(dirPath string, pathType, addType int) (bool, int, error) {
 		uintptr(pathType), // 0 system - 1 user
 		uintptr(addType),  // 0 end - 1 start
 	)
-	fmt.Printf("ret: %v\n", ret)
-	fmt.Printf("err: %v\n", err)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	//fmt.Printf("ret: %v\n", ret)
 
 	switch ret {
 	case 0:
@@ -73,7 +78,27 @@ func AddDirToPath(dirPath string, pathType, addType int) (bool, int, error) {
 		fmt.Println("Directory already in PATH")
 		return true, int(ret), nil
 	default:
-		fmt.Printf("Error adding to PATH ret: %d\n", ret)
+		fmt.Printf("Error adding to PATH: %d\n", ret)
 		return false, int(ret), nil
 	}
+}
+
+func getDLLPath() (string, error) {
+	exePath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	exeDir := filepath.Dir(exePath)
+	var archDir string
+	if unsafe.Sizeof(uintptr(0)) == 8 {
+		archDir = "x86_64"
+	} else {
+		archDir = "i386"
+	}
+	path := filepath.Join(exeDir, archDir, "PathMgr.dll")
+	// this block is for development using go run main.go
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		path = filepath.Join("dist/PathMgr", archDir, "PathMgr.dll")
+	}
+	return path, nil
 }
