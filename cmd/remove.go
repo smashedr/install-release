d922fe427e588fc33b84dca6050ec1cd2e4d4460
Shane <6071159+smashedr@users.noreply.github.com>
2026-02-06 23:11:10 -0800

    Add Remove Command

--- cmd/remove.go
+++ cmd/remove.go
@@ -1,9 +1,14 @@
 package cmd
 
 import (
+	"fmt"
+	"github.com/charmbracelet/huh"
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"runtime"
 )
 
 var removeCmd = &cobra.Command{
@@ -14,8 +19,54 @@ var removeCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		binPath := viper.GetString("bin")
 		log.Debug("removeCmd:", "args", args, "binPath", binPath)
+		//noConfirm, _ := cmd.Flags().GetBool("yes")
+		//log.Debug("Flags", "noConfirm", noConfirm)
 
-		log.Fatal("INOP: The remove command is not yet functional...")
+		if len(args) == 0 {
+			_ = cmd.Help()
+			return
+		}
+
+		name := args[0]
+		log.Infof("name: %v", name)
+
+		path := filepath.Join(binPath, name)
+		log.Debugf("path: %v", path)
+		info, _ := os.Stat(path)
+		log.Debugf("info: %v", info)
+		if info == nil && runtime.GOOS == "windows" {
+			path += ".exe"
+			info, _ = os.Stat(path)
+		}
+		if info == nil {
+			log.Fatalf("App not found: %v", name)
+		}
+		log.Infof("info: %v", info.Name())
+		if info.IsDir() {
+			log.Fatalf("Is a directory: %v", path)
+		}
+
+		var confirm = true
+		form := huh.NewConfirm().
+			Title(fmt.Sprintf("Remove: %s", path)).
+			Affirmative("Remove!").
+			Negative("Cancel").
+			Value(&confirm).
+			WithTheme(huh.ThemeDracula())
+		err := form.Run()
+		if err != nil {
+			log.Fatalf("prompt error: %v", err)
+		}
+		log.Infof("confirm: %v", confirm)
+		if !confirm {
+			return
+		}
+
+		err = os.Remove(path)
+		if err != nil {
+			log.Fatalf("prompt error: %v", err)
+		}
+		fmt.Printf("Removed: %v\n", name)
 	},
 }
 
