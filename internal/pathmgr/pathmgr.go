1bbd09516c5a2e0785ac97ed5b405942dcd48d62
SmAsHeD <6071159+smashedr@users.noreply.github.com>
2026-02-13 22:17:21 -0800

    Cleanup Path Management (#27)

--- internal/pathmgr/pathmgr.go
+++ internal/pathmgr/pathmgr.go
@@ -1,25 +1,68 @@
 package pathmgr
 
 import (
-	"fmt"
 	"github.com/charmbracelet/log"
-	"github.com/smashedr/install-release/internal/styles"
 	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
-func CheckBinPath(binPath string) {
-	if os.Getenv("DOCKER") == "true" {
-		log.Infof("Skipping CheckBinPath in DOCKER.")
-		return
+func IsDirInPath(dirPath string) (found bool, err error) {
+	log.Debug("IsDirInPath", "dirPath", dirPath)
+
+	// NOTE: Determine how to handle PATH in DOCKER
+	if os.Getenv("DOCKER") != "true" {
+		log.Infof("IsDirInPath is Disabled in DOCKER")
+		return true, nil
 	}
-	//fmt.Printf("CheckBinPath: %s\n", binPath)
-	result, _, _ := IsDirInPath(binPath)
-	//fmt.Printf("result: %v\n", result)
-	if !result {
-		log.Warnf("bin not in PATH: %v", binPath)
-		log.Printf("To add bin to PATH, run:\n")
-		fmt.Println(styles.Command.Render(fmt.Sprintf("ir path add %s", binPath)))
-	} else {
-		log.Infof("Found bin in PATH: %s", binPath)
+
+	pathEnv := os.Getenv("PATH")
+	paths := strings.Split(pathEnv, string(os.PathListSeparator))
+	log.Debugf("paths: %v", paths)
+	absDir, err := filepath.Abs(dirPath)
+	log.Info("IsDirInPath", "dirPath", dirPath)
+	if err != nil {
+		return false, err
 	}
+
+	for _, p := range paths {
+		absPath, err := filepath.Abs(p)
+		log.Debugf("absPath: %v", absPath)
+		if err != nil {
+			continue
+		}
+		if pathsEqual(absPath, absDir) {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
+
+func pathsEqual(a, b string) bool {
+	if runtime.GOOS == "windows" {
+		return strings.EqualFold(a, b)
+	}
+	return a == b
+}
+
+//func CheckBinPath(binPath string) {
+//	log.Debug("CheckBinPath", "binPath", binPath)
+//	if os.Getenv("DOCKER") == "true" {
+//		log.Infof("Skipping CheckBinPath in DOCKER.")
+//		return
+//	}
+//	result, _, err := IsDirInPath(binPath)
+//	log.Debugf("result: %v", result)
+//	if err != nil {
+//		log.Fatal(err) // TODO: Confirm Fatal is not an issue...
+//	}
+//	if !result {
+//		log.Warnf("bin not in PATH: %v", binPath)
+//		log.Printf("To add bin to PATH, run:\n")
+//		fmt.Println(styles.Command.Render(fmt.Sprintf("ir path add %s", binPath)))
+//	} else {
+//		log.Infof("Found bin in PATH: %s", binPath)
+//	}
+//}
